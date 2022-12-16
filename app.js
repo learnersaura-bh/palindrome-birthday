7238f33a0fd3e2ff79347264dbc74493e90b822e
learnersaura-bh <119805882+learnersaura-bh@users.noreply.github.com>
2022-12-16 21:32:22 +0530

    fixed-error

--- app.js
+++ app.js
@@ -14,19 +14,18 @@ function isPalindrome(str){
 return str === reverse;
 }
 
-
 function changeDateToString(date){
 
     dateStr = { day : "" , month: "" , year : ""}
 
-    if (dateStr.day < 10){
+    if (date.day < 9){
        dateStr.day =  '0' + date.day
     }
     else{dateStr.day = date.day.toString(); }
 
     
-    if (dateStr.month < 10){
-        dateStr.month =  '0' + date.month
+    if (date.month < 10){
+        dateStr.month = '0' + date.month;
      }
      else{dateStr.month = date.month.toString(); }
 
@@ -66,15 +65,83 @@ return flag ;
 
 }
 
+function isLeapYear(year){
+    if(year%400 === 0){
+        return true;
+    }
+    if(year%100 === 0){
+        return true;
+    }
+    if(year%4 === 0){
+        return true;
+    }
+    
+   else return false;
+}
 
+function getNextDate(date){
+   var day = date.day + 1;
+   var month = date.month;
+   var year = date.year;
+
+   var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+   if (month === 2){
+    if(isLeapYear(year)){
+        if(day > 29){
+            day = day+1
+            month++
+        }
+
+    }
+    else {
+        if(day > 28){
+            day = day+1;
+            month++
+        }
+    }
+}
+   else {
+    if(day > daysInMonth[month-1]){
+        day=1;
+        month++;
+    }
+   }
+   if(month > 12){
+    month = 1;
+    year++
+   }
+   return{
+    day : day,
+    month : month,
+    year : year,
+   };
+}
+
+function getNextPalindromeDate(date){
+    var ctr = 0;
+    var nextDate = getNextDate(date);
+
+    while(1){
+        ctr = ctr+1
+        var isPalindrome = checkPalindromeForAllFormat(nextDate);
+        if (isPalindrome){
+            break;
+        }
+        else{nextDate = getNextDate(nextDate);}
+    }
+
+return [ctr, nextDate]
+
+}
 
 
 
 
 var date = {
-    day : 22,
-    month : 2,
-    year : 2022,
+    day : 10,
+    month : 1,
+    year : 2021,
 }
 
-console.log(checkPalindromeForAllFormat(date));
+console.log(getNextPalindromeDate(date));
